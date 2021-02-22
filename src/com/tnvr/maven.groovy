7d2ac537a83441c879458470120cba157e3b5330
tnvrgit <37243335+tnvrgit@users.noreply.github.com>
2021-02-22 13:28:59 +0530

    Create maven.groovy

---
+++ src/com/tnvr/maven.groovy
@@ -0,0 +1,8 @@
+package com.tnvr
+class Utilities implements Serializable {
+  def steps
+  Utilities(steps) {this.steps = steps}
+  def mvn(args) {
+    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+  }
+}

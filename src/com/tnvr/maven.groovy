f733032d5c6f73aa598daf71124fb38ef01e573c
tnvrgit <37243335+tnvrgit@users.noreply.github.com>
2021-02-23 17:46:22 +0530

    Update maven.groovy

--- src/com/tnvr/maven.groovy
+++ src/com/tnvr/maven.groovy
@@ -1,7 +1,9 @@
 package com.tnvr
-class maven {
+class maven1 {
   def steps
-  maven(steps) {this.steps = steps}
+  maven(steps) {
+    this.steps = steps
+  }
   def mvn(args) {
     steps.sh "${steps.tool 'maven'}/bin/mvn -o ${args}"
   }

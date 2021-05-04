e555be08610619bcd52483ee663c1d2962a99070
tnvrgit <37243335+tnvrgit@users.noreply.github.com>
2021-05-05 01:01:59 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,9 +1,6 @@
 def cal(String ProjectKey) {
-  script: {mvn sonar:sonar \
-  -Dsonar.projectKey=${ProjectKey} \
-  -Dsonar.host.url=http://192.168.40.128:9001 \
-  -Dsonar.login=15e8fbeab0615b42bc7155ab446639aaeabe87fe
-  }
+  script: 'mvn sonar:sonar -Dsonar.projectKey=${ProjectKey} -Dsonar.host.url=http://192.168.40.128:9001 -Dsonar.login=15e8fbeab0615b42bc7155ab446639aaeabe87fe'
+  
 }
 
 return this

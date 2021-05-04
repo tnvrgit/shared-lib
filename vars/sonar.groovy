c158bdfe87d76989126891a75de2ed62cc286355
tnvrgit <37243335+tnvrgit@users.noreply.github.com>
2021-05-05 01:00:35 +0530

    Create sonar.groovy

---
+++ vars/sonar.groovy
@@ -0,0 +1,8 @@
+def cal(String ProjectKey) {
+  mvn sonar:sonar \
+  -Dsonar.projectKey=${ProjectKey} \
+  -Dsonar.host.url=http://192.168.40.128:9001 \
+  -Dsonar.login=15e8fbeab0615b42bc7155ab446639aaeabe87fe
+}
+
+return this
